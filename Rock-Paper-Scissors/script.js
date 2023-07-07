8babdb8d49a08b32ed923292876835da151c0942
Kee Seow Han <seow.han@u.nus.edu>
2023-07-07 19:43:18 +0800

    Implement UI for game

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,4 +1,25 @@
 const moves = ["rock", "paper", "scissors"];
+let countPlayer = 0;
+let countComputer = 0;
+const toWin = 3;
+const resultsText = document.querySelector(".results-text");
+const score = document.querySelector(".score");
+const titleGif = document.querySelector(".title-gif");
+
+function gameEnded() {
+  if (countComputer >= toWin) {
+    titleGif.src = "./Images/cat-win.png";
+    return "Computer has won!";
+  }
+
+  if (countPlayer >= toWin) {
+    titleGif.src = "./Images/cat-lose.png";
+    return "Player has won!";
+  }
+
+  return "";
+}
+
 function getComputerChoice() {
   const rand = Math.floor(Math.random() * 3);
   return rand;
@@ -9,11 +30,12 @@ function parseMove(playerMove) {
 }
 
 function playRound(playerMove, computerMove) {
+  console.log(playerMove, computerMove);
   if (playerMove == computerMove) {
     return 0;
   }
 
-  if (playerMove > computerMove && computerMove != 0) {
+  if (playerMove - computerMove == 1) {
     return 1;
   }
 
@@ -24,40 +46,76 @@ function playRound(playerMove, computerMove) {
   return -1;
 }
 
-function game() {
-  let countPlayer = 0;
-  let countComputer = 0;
+function tabulatePoints(roundResults) {
+  if (roundResults == 0) {
+    resultsText.textContent = "This round is a draw!";
+    return;
+  }
 
-  while (true) {
-    if (countPlayer > 2) {
-      console.log("Player has won!");
-      break;
+  if (roundResults > 0) {
+    countPlayer++;
+    if (countPlayer < toWin) {
+      resultsText.textContent = "Player has won this round!";
     }
-
-    if (countComputer > 2) {
-      console.log("Computer has won!");
-      break;
+  } else if (roundResults < 0) {
+    countComputer++;
+    if (countComputer < toWin) {
+      resultsText.textContent = "Computer has won this round!";
     }
+  }
 
-    let playerMove;
-    do {
-      playerMove = parseMove(
-        prompt("Choose rock, paper, or scissors (Case insensitive)")
-      );
-    } while (playerMove == -1);
+  score.textContent = `${countPlayer} - ${countComputer}`;
+}
 
-    const res = playRound(playerMove, getComputerChoice());
+function handleClick(playerMove) {
+  if (parseMove == -1) {
+    return;
+  }
 
-    if (res > 0) {
-      console.log("Player won this round!");
-      countPlayer++;
-    } else if (res < 0) {
-      console.log("Computer won this round!");
-      countComputer++;
-    } else {
-      console.log("This round was a draw!");
-    }
+  const roundResults = playRound(parseMove(playerMove), getComputerChoice());
+  console.log(roundResults);
+  tabulatePoints(roundResults);
+
+  const gameResults = gameEnded();
+
+  if (gameResults != "") {
+    resultsText.textContent =
+      gameResults + "\nClick below to start a new game!";
+
+    // remove all event listeners
+    var old_element = document.querySelector(".options");
+    var new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+
+    newGame();
   }
 }
 
-game();
+function init() {
+  countPlayer = 0;
+  countComputer = 0;
+
+  score.textContent = "0 - 0";
+  resultsText.textContent = "Your move!";
+  titleGif.src = "./Images/cat-fight.gif";
+  // add all event listeners
+  document.querySelectorAll(".option-image").forEach((e) =>
+    e.addEventListener("click", (e) => {
+      handleClick(e.target.name);
+    })
+  );
+}
+
+function newGame() {
+  const results = document.querySelector(".results");
+  let btn = document.createElement("button");
+  btn.classList.add("new-game");
+  btn.textContent = "New game";
+  results.appendChild(btn);
+  btn.addEventListener("click", (e) => {
+    init();
+    btn.remove();
+  });
+}
+
+init();
